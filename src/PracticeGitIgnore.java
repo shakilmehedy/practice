4d35ce52ffc33a18f0701994e3e5e5491cedaa53
shakil <shakilmehedy@gmail.com>
2024-11-05 15:47:06 +0600

    ok

---
+++ src/PracticeGitIgnore.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PracticeGitIgnore {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please input");
+            String message1 = scanner.nextLine();
+            String message2=scanner.nextLine();
+            System.out.println(message1+" ".concat(message2));
+
+
+            scanner.close();
+
+        }
+    }
+
+
